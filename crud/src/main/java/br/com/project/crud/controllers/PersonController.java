aaa3d50f7f2f43182e98844502a2b5b6c09c0b7d
Juliano Salgueiro <jsalgueiro@daitangroup.com>
2018-04-11 16:49:29 -0300

    read and update controllers

--- crud/src/main/java/br/com/project/crud/controllers/PersonController.java
+++ crud/src/main/java/br/com/project/crud/controllers/PersonController.java
@@ -3,9 +3,12 @@ package br.com.project.crud.controllers;
 import br.com.project.crud.daos.PersonRepository;
 import br.com.project.crud.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class PersonController {
@@ -20,4 +23,23 @@ public class PersonController {
         personRepository.save(person);
         return person.toString() + "Cadastrado com sucesso ";
     }
+
+    @RequestMapping("/read")
+    public Iterable<Person> read(){
+        return personRepository.findAll();
+    }
+
+    @RequestMapping("/update/{id}")
+    public Person update (@PathVariable(value = "id")long id, @RequestParam(value = "name", defaultValue = "teste")String name, @RequestParam(value = "country",defaultValue = "teste")String country){
+
+        Optional<Person> optional = personRepository.findById(id);
+        Person person = optional.get();
+        person.setName(name);
+        person.setCountry(country);
+
+        Person updatedperson = personRepository.save(person);
+        return updatedperson;
+    }
+
+
 }
